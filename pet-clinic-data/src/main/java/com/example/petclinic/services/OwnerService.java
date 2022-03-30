f76a2d205f4a17c9f95381b73972c142bafd5120
Alpha BARRY <alpha.bbary@gmail.com>
2022-03-30 11:21:02 +0200

    Create Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package com.example.petclinic.services;
+
+import com.example.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastname(String lastname);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
