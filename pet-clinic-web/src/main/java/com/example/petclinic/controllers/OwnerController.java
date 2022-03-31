7d98f3c13b7ca192833cb43e3100cd30fc5fc70c
Alpha BARRY <alpha.bbary@gmail.com>
2022-03-31 09:46:18 +0200

    Add owner index page and controller. Closes #7.

---
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.example.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+    @RequestMapping({"", "/", "/index"})
+    public String list(){
+        return "/owners/index";
+    }
+}
